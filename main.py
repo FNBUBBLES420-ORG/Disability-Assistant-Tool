a8914db5f625c3fd668854a0521ff14448aac79f
Bubbles The Dev <152947339+KernFerm@users.noreply.github.com>
2026-03-05 01:19:08 -0500

    Create main.py
    Signed-off-by: Bubbles The Dev <152947339+KernFerm@users.noreply.github.com>

---
+++ main.py
@@ -0,0 +1,12 @@
+print("🚧 Coming Soon")
+print("This repository will include example files and documentation shortly.")
+print("Please check back later for updates.\n")
+
+print("⚙️ This software is currently being developed.")
+print("If you would like to contribute, please join the Discord server.")
+
+print("\nSteps to get involved:")
+print("1. Join the Discord server.")
+print("2. Choose your roles upon joining.")
+print("3. Ping 'bubblesthedev' and create a support ticket.")
+print("4. Contributors will be required to sign an NDA agreement before working on the project.")
